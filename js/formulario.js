213b8ed05ea1f9e90816be99a2a3885f28666ef2
Alucia <analu1478@gmail.com>
2015-11-24 10:03:07 -0500

    add formulario

---
+++ js/formulario.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	(function(d, t){
+        var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+	        g.src = "http://es.foxyform.com/js.php?id=248578&sec_hash=69558f6b088&width=350px";
+	        s.parentNode.insertBefore(g, s);
+    }(document, "script"));
+});
